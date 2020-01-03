bbb66e30b391c002b0010b95551f24e1b3bacd20
Trần Nguyên <32108974+nguyentrn@users.noreply.github.com>
2020-01-04 03:33:54 +0700

    refactor upsert

--- model.js
+++ model.js
@@ -6,6 +6,7 @@
 //
 const axios = require("axios");
 const pg = require("./database");
+const insert = require("./insertCoinInfo");
 const delay = time => {
   return new Promise(function(resolve) {
     setTimeout(resolve, time);
@@ -23,37 +24,24 @@ const scraper = async model => {
       const link = `https://www.binance.com/api/v1/klines?symbol=${
         model.symbol
       }USDT&interval=1m${
-        lastTimestamp ? `&endTime=${lastTimestamp}` : ""
+        lastTimestamp ? `&endTime=${lastTimestamp - 60000}` : ""
       }&limit=1000`;
-      console.log(i, link);
+      console.log(i, model.symbol);
       const coins = await axios(link);
       if (coins.data.length < 1000) {
         break;
       }
       if (coins.data.length) {
-        coins.data.forEach(async dt => {
-          const scrapedA = await pg(model.name)
-            .where({ time: new Date(dt[0]) })
-            .select("*");
-          const scraped = scrapedA[0];
-          // console.log(scraped);
-          if (!scraped) {
-            await pg(model.name).insert({
-              time: new Date(dt[0]),
-              open: dt[1],
-              high: dt[2],
-              low: dt[3],
-              close: dt[4],
-              volume: dt[5],
-              close_time: new Date(dt[6]),
-              quote: dt[7],
-              number_of_trades: dt[8],
-              taker_buy_base: dt[9],
-              taker_buy_quote: dt[10],
-              ignore: dt[11]
-            });
-          }
-        });
+        // coins.data.forEach(async dt => {
+        // const scrapedA = await pg(model.name)
+        //   .where({ time: new Date(dt[0]) })
+        //   .select("*");
+        // const scraped = scrapedA[0];
+        // console.log(scraped);
+        // if (!scraped) {
+        await insert(model, coins.data);
+        // }
+        // });
       }
       await delay(10000);
     }
